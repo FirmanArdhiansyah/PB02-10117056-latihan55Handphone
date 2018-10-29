a2418aee6d889b35be70575fb0326c7e0e0471c1
FirmanArdhiansyah <firmanardhi14@gmail.com>
2018-10-29 16:02:04 +0700

    Latihan55

---
+++ src/Android.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nama : Firman Ardhiansyah
+ *         Kelas : IF-2 (PB02)
+ *         NIM : 10117056
+ * Deskripsi Program : Program Tentang Informasi sepuatar handphone
+ */
+public class Android extends Handphone{
+
+    private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+}
